3cdc5bce60a838a9a6e8870f441cc9a42bccb376
ddururiiiiiii <o244227@daum.net>
2024-11-17 17:59:33 +0900

    올바른 괄호

---
+++ programmers/level2/241117 올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = True
+    arr = []
+    for i in s:
+        if i == '(':
+            arr.append(i)
+        else :
+            if arr :
+                arr.pop()
+            else :
+                return False
+    if arr:
+        return False
+
+    return True
